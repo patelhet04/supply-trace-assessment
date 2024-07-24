5acaff74200c2a4149149b7911f91f2680c80e11
patelhet04 <hetpatel.hp4199@gmail.com>
2024-07-24 02:28:12 -0400

    Added Company list page with searchbar component

--- src/components/CompanyCard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CompanyCard = ({ companyData }) => {
-  return <div>CompanyCard</div>;
-};
-
-export default CompanyCard;
--- src/pages/CompanyList/CompanyList.jsx
+++ src/pages/CompanyList/CompanyList.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import CompanyCard from "../../components/CompanyCard";
-
+import CompanyCard from "../../components/CompanyCard/CompanyCard";
+import "./companylist.css";
+import SearchBar from "../../components/SearchBar/SearchBar";
 const CompanyList = () => {
   const companies = [
     { id: 1, companyName: "ABC" },
     { id: 2, companyName: "ABC" },
+    { id: 3, companyName: "ABC" },
+    { id: 4, companyName: "ABC" },
     { id: 3, companyName: "ABC" },
     { id: 4, companyName: "ABC" },
   ];
@@ -12,9 +15,12 @@ const CompanyList = () => {
   return (
     <>
       <h2>Browse Companies</h2>
-      {companies.map((company) => (
-        <CompanyCard companyData={company} />
-      ))}
+      <SearchBar />
+      <div className="company-container">
+        {companies.map((company) => (
+          <CompanyCard companyData={company} />
+        ))}
+      </div>
     </>
   );
 };
