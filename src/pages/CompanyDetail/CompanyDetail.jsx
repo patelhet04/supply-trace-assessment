d204cb24b68b48a563502d20b07a8371e0ca50d9
patelhet04 <hetpatel.hp4199@gmail.com>
2024-07-24 13:03:47 -0400

    Added map component

--- src/pages/CompanyDetail/CompanyDetail.jsx
+++ src/pages/CompanyDetail/CompanyDetail.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import "./companydetail.css";
+import { Link } from "react-router-dom";
+import CompanyMap from "../../components/CompanyMap/CompanyMap";
 const CompanyDetail = () => {
-  return <div>CompanyDetail</div>;
+  const position = [51.505, -0.09];
+  return (
+    <>
+      <Link
+        // key={company.company_id}
+        to={`/`}
+        style={{ textDecoration: "none" }}
+      >
+        <button className="back-btn">Back to list</button>
+      </Link>
+      <header className="company-header">
+        <strong>Tech Innovators Inc.</strong>
+        <hr />
+      </header>
+      <div className="company-details">
+        <div className="details-left">
+          <div className="address">
+            <h4>Address</h4>
+            <p>123 Tech Lane, Innovation City, USA</p>
+          </div>
+          <div className="locations">
+            <h4>Locations</h4>
+            <span className="badge">Location 1</span>
+            <span className="badge">Location 2</span>
+            <span className="badge">Location 3</span>
+            <span className="badge">Location 3</span>
+            <span className="badge">Location 3</span>
+            <span className="badge">Location 3</span>
+          </div>
+        </div>
+        <div className="details-right">
+          <div className="map">
+            {/* Here you would integrate your map component, e.g., Leaflet or Google Maps */}
+            <CompanyMap position={position} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CompanyDetail;
